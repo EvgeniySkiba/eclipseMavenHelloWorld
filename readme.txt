3c18659a6a2cb1e03ad858373ee5bf43043013c9
EvgeniySkiba <Skiba.Evgeniy@hotmail.com>
2017-05-03 21:34:22 +0400

    initial commit

---
+++ readme.txt
@@ -0,0 +1,4 @@
+for run application :
+1. go to /target folder (as a result commands maven-clean , maven - install )
+2. run as a command 
+java -jar wpp-1.0.jarn maven-test-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/se/learn/maven/maven_test/App.java
@@ -0,0 +1,19 @@
+package com.se.learn.maven.maven_test;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Scanner in = new Scanner(System.in);
+        System.out.print("press any key to continue ... ");
+        String name = in.nextLine();       
+    }
+}
